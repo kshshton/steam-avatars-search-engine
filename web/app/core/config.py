a3df83be0febd498f1feb75920aae241597dc9c0
kshshton <michkrzyszton@gmail.com>
2026-05-07 00:36:28 +0200

    Remove API_V1_PREFIX

--- web/app/core/config.py
+++ web/app/core/config.py
@@ -3,9 +3,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Steam avatars search engine"
-    API_V1_PREFIX: str = "/api/v1"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
 
 settings = Settings()
